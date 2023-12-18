f191d99eeae24e279d4ca736457f9d490b2ab800
Alex <alex.j.dunne1@gmail.com>
2023-12-18 00:37:56 +0000

    add vs and dest rules

--- terraform/istio/destination_rules.tf
+++ terraform/istio/destination_rules.tf
@@ -1,28 +1,28 @@
-# resource "kubernetes_manifest" "my_destination_rule" {
-#   depends_on = [helm_release.istiod]
-#   manifest = {
-#     apiVersion = "networking.istio.io/v1alpha3"
-#     kind       = "DestinationRule"
-#     metadata = {
-#       name      = "my-destination-rule"
-#       namespace = var.istio_namespace
-#     }
-#     spec = {
-#       host = "fastapi-service.fastapi-app.svc.cluster.local"
-#       subsets = [
-#         {
-#           name   = "v1"
-#           labels = {
-#             version = "v1"
-#           }
-#         },
-#         {
-#           name   = "v2"
-#           labels = {
-#             version = "v2"
-#           }
-#         }
-#       ]
-#     }
-#   }
-# }
+resource "kubernetes_manifest" "my_destination_rule" {
+  depends_on = [helm_release.istiod]
+  manifest = {
+    apiVersion = "networking.istio.io/v1alpha3"
+    kind       = "DestinationRule"
+    metadata = {
+      name      = "my-destination-rule"
+      namespace = var.istio_namespace
+    }
+    spec = {
+      host = "fastapi-service.fastapi-app.svc.cluster.local"
+      subsets = [
+        {
+          name   = "v1"
+          labels = {
+            version = "v1"
+          }
+        },
+        {
+          name   = "v2"
+          labels = {
+            version = "v2"
+          }
+        }
+      ]
+    }
+  }
+}
--- terraform/istio/virtual_services.tf
+++ terraform/istio/virtual_services.tf
@@ -1,48 +1,48 @@
-# resource "kubernetes_manifest" "my_virtual_service" {
-#   depends_on = [helm_release.istiod]
-#   manifest = {
-#     apiVersion = "networking.istio.io/v1alpha3"
-#     kind       = "VirtualService"
-#     metadata = {
-#       name      = "my-virtual-service"
-#       namespace = var.istio_namespace
-#     }
-#     spec = {
-#       hosts = ["*"]
-#       gateways = ["istio-gateway"] 
-#       http = [
-#         {
-#           match = [
-#             {
-#               uri = {
-#                 prefix = "/"
-#               }
-#             }
-#           ]
-#             route = [
-#                 {
-#                 destination = {
-#                     host = "fastapi-service.fastapi-app.svc.cluster.local"
-#                     subset = "v1"
-#                     port = {
-#                     number = 80 
-#                     }
-#                 }
-#                     weight = 80
-#                 },
-#                 {
-#                     destination = {
-#                     host = "fastapi-service.fastapi-app.svc.cluster.local"
-#                     subset = "v2"
-#                     port = {
-#                         number = 80 
-#                     }
-#                     }
-#                     weight = 20
-#                 }
-#             ]
-#     }
-#     ]
-#   }
-# }
-# }
+resource "kubernetes_manifest" "my_virtual_service" {
+  depends_on = [helm_release.istiod]
+  manifest = {
+    apiVersion = "networking.istio.io/v1alpha3"
+    kind       = "VirtualService"
+    metadata = {
+      name      = "my-virtual-service"
+      namespace = var.istio_namespace
+    }
+    spec = {
+      hosts = ["*"]
+      gateways = ["istio-gateway"] 
+      http = [
+        {
+          match = [
+            {
+              uri = {
+                prefix = "/"
+              }
+            }
+          ]
+            route = [
+                {
+                destination = {
+                    host = "fastapi-service.fastapi-app.svc.cluster.local"
+                    subset = "v1"
+                    port = {
+                    number = 80 
+                    }
+                }
+                    weight = 80
+                },
+                {
+                    destination = {
+                    host = "fastapi-service.fastapi-app.svc.cluster.local"
+                    subset = "v2"
+                    port = {
+                        number = 80 
+                    }
+                    }
+                    weight = 20
+                }
+            ]
+    }
+    ]
+  }
+}
+}
