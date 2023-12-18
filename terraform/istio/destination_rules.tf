7dfa58e5a567915be9bb16b6e165235e35a66f2f
Alex <alex.j.dunne1@gmail.com>
2023-12-18 10:56:20 +0000

    add two svcs

--- terraform/istio/destination_rules.tf
+++ terraform/istio/destination_rules.tf
@@ -1,21 +1,38 @@
-resource "kubernetes_manifest" "my_destination_rule" {
+resource "kubernetes_manifest" "my_destination_rule_v1" {
   depends_on = [helm_release.istiod]
   manifest = {
     apiVersion = "networking.istio.io/v1alpha3"
     kind       = "DestinationRule"
     metadata = {
-      name      = "my-destination-rule"
+      name      = "my-destination-rule-v1"
       namespace = var.istio_namespace
     }
     spec = {
-      host = "fastapi-service.fastapi-app.svc.cluster.local"
+      host = "fastapi-service-v1.fastapi-app.svc.cluster.local"
       subsets = [
         {
           name   = "v1"
           labels = {
             version = "v1"
           }
-        },
+        }
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "my_destination_rule_v2" {
+  depends_on = [helm_release.istiod]
+  manifest = {
+    apiVersion = "networking.istio.io/v1alpha3"
+    kind       = "DestinationRule"
+    metadata = {
+      name      = "my-destination-rule-v2"
+      namespace = var.istio_namespace
+    }
+    spec = {
+      host = "fastapi-service-v2.fastapi-app.svc.cluster.local"
+      subsets = [
         {
           name   = "v2"
           labels = {
--- terraform/kubernetes-config/services.tf
+++ terraform/kubernetes-config/services.tf
@@ -1,11 +1,28 @@
-resource "kubernetes_service" "fastapi-service" {
+resource "kubernetes_service" "fastapi-service-v1" {
   metadata {
-    name = "fastapi-service"
+    name = "fastapi-service-v1"
     namespace = kubernetes_namespace.fastapi.metadata.0.name
   }
   spec {
     selector = {
-      app = "fastapi-app"
+      app = "fastapi-app-v1"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_service" "fastapi-service-v2" {
+  metadata {
+    name = "fastapi-service-v2"
+    namespace = kubernetes_namespace.fastapi.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = "fastapi-app-v2"
     }
     port {
       port        = 80
